0857fc1d48f45fb1f42d34d9b518b1462d8d3c1c
github username <github email address>
2018-11-13 09:29:16 -0500

    add new user page working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 class UsersController < ApplicationController
+
+  before_action :get_user, only: [:show]
   def index
   end
 
   def show
-    @user = User.find(params[:id])
+    
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :zodiac_sign_id)
+    end
+
+    def get_user
+      @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :new, :create]
   resources :zodiac_signs, only: [:index, :show]
   resources :madlibs, only: [:new]
 end
