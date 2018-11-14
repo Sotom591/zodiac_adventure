0c83e2062ca60933755c7ce06ed73f90be0faaa2
github username <github email address>
2018-11-14 16:57:35 -0500

    one madlib working

--- app/models/madlib.rb
+++ app/models/madlib.rb
@@ -1,22 +1,21 @@
 class Madlib < ApplicationRecord
   belongs_to :user
 
-  def prediction_input
-
-  end
-
-  def prediction(params = {})
-    body_part = params.fetch(:body_part)
-    verb_ing = params.fetch(:verb_ing)
-    anim = params.fetch(:anim)
-    adj = params.fetch(:adj)
-    job = params.fetch(:job)
-    adj2 = params.fetch(:adj2)
-    randname = params.fetch(:ranname)
 
 
-"It is all so clear now. When you first meet someone new, you look at their #{body_part}. If you like what you see, it can be hard for you not to begin #{verb_ing}. But it’s important instead to introduce yourself by your name. Later into your relationship though you can ask your sweetheart to call you by your favorite pet name, #{anim}.
+  def prediction(words)
+    body_part = words[:body_part]
+    verb_ing = words[:verb_ing]
+    anim = words[:anim]
+    adj = words[:adj]
+    job = words[:job]
+    adj2 = words[:adj2]
+    randname = words[:randname]
 
-Remember, it is better for you to consider dating #{adj} types of people. In the past, you only dated #{job}’s. It’s time to branch out as it will lead you to #{adj2} love. Oh, and watch out for #{randname}s’."
+
+"It is all so clear now. When you first meet someone new, you look at their #{body_part.downcase}.
+If you like what you see, it can be hard for you not to begin #{verb_ing.downcase}. But it’s important instead to introduce yourself by your name. Later into your relationship though you can ask your sweetheart to call you by your favorite pet name, #{anim.downcase}.
+
+Remember, it is better for you to consider dating #{adj.downcase} types of people. In the past, you only dated #{job.downcase}’s. It’s time to branch out as it will lead you to #{adj2.downcase} love. Oh, and watch out for #{randname.downcase}s’."
   end
 end
