56a5fe21bc18382ed3b82616fa54ee7fa6e584b7
github username <github email address>
2018-11-15 13:08:11 -0500

    css stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
+  
   resources :users, only: [:show, :new, :create, :edit, :update]
   resources :zodiac_signs, only: [:index, :show]
   resources :madlibs, only: [:index, :new, :create, :show]
   resources :constellations, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
